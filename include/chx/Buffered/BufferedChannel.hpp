113127a9cdb7b173daf2dc1c5f0e47f0feda5a1b
Vorex075 <roberto@losgimenez.com>
2025-08-26 19:36:42 +0100

    Deleted data handler for buffered channel

--- include/chx/Buffered/BufferedChannel.hpp
+++ include/chx/Buffered/BufferedChannel.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "../channel.hpp"
-#include "BufferedDataHandler.hpp"
+#include "circular_queue.hpp"
+#include <condition_variable>
+#include <mutex>
 #include <utility>
 
 namespace chx::buffered {
@@ -9,7 +11,7 @@ namespace chx::buffered {
 template <typename T, std::size_t Capacity>
 class Channel : public chx::Channel<T> {
 public:
-  Channel() : chx::Channel<T>(), data_() {}
+  Channel() : chx::Channel<T>(), closed(false) {}
   Channel(const Channel<T, Capacity> &ch) = delete;
   ~Channel() = default;
 
@@ -27,7 +29,6 @@ public:
   Channel<T, Capacity> &operator=(const Channel<T, Capacity> &ch) = delete;
 
 private:
-  DataHandler<T, Capacity> data_;
   template <typename U>
     requires std::constructible_from<T, U &&>
   std::expected<void, Error> send_(U &&value);
@@ -35,20 +36,26 @@ private:
   template <typename U>
     requires std::constructible_from<T, U &&>
   std::expected<void, Error> try_send_(U &&value);
+
+  mutable std::mutex mutex;
+  std::condition_variable not_empty;
+  std::condition_variable not_full;
+  CircularQueue<T, Capacity> queue;
+  bool closed = false;
 };
 
 template <typename T, std::size_t Capacity> void Channel<T, Capacity>::close() {
-  std::lock_guard lock(this->data_.mutex);
-  this->data_.closed = true;
-  this->data_.receiver_entrance.notify_all();
-  this->data_.sender_entrance.notify_all();
+  std::lock_guard lock(this->mutex);
+  this->closed = true;
+  this->receiver_entrance.notify_all();
+  this->sender_entrance.notify_all();
   return;
 }
 
 template <typename T, std::size_t Capacity>
 bool Channel<T, Capacity>::is_closed() const {
-  std::lock_guard lock(this->data_.mutex);
-  return this->data_.closed == true;
+  std::lock_guard lock(this->mutex);
+  return this->closed == true;
 }
 
 template <typename T, std::size_t Capacity>
@@ -75,14 +82,14 @@ template <typename T, std::size_t Capacity>
 template <typename U>
   requires std::constructible_from<T, U &&>
 std::expected<void, Error> Channel<T, Capacity>::send_(U &&value) {
-  std::unique_lock lock(this->data_.mutex);
-  this->data_.not_full.wait_(
-      lock, [&] { return !this->data_.queue.is_full() || this->data_.closed; });
-  if (this->data_.closed) {
+  std::unique_lock lock(this->mutex);
+  this->not_full.wait(lock,
+                      [&] { return !this->queue.is_full() || this->closed; });
+  if (this->closed) {
     return std::unexpected("channel closed");
   }
-  this->data_.queue.push(std::forward<T>(value));
-  this->data_.not_empty.notify_one();
+  this->queue.push(std::forward<T>(value));
+  this->not_empty.notify_one();
   return {};
 }
 
@@ -90,46 +97,45 @@ template <typename T, std::size_t Capacity>
 template <typename U>
   requires std::constructible_from<T, U &&>
 std::expected<void, Error> Channel<T, Capacity>::try_send_(U &&value) {
-  std::unique_lock lock(this->data_.mutex);
-  if (this->data_.closed) {
+  std::unique_lock lock(this->mutex);
+  if (this->closed) {
     return std::unexpected("channel closed");
   }
-  if (this->data_.queue.is_full()) {
+  if (this->queue.is_full()) {
     return std::unexpected("cannot send inmediatly since the buffer is full");
   }
-  this->data_.queue.push(std::forward<T>(value));
-  this->data_.not_empty.notify_one();
+  this->queue.push(std::forward<T>(value));
+  this->not_empty.notify_one();
   return {};
 }
 
 template <typename T, std::size_t Capacity>
 std::expected<T, Error> Channel<T, Capacity>::receive() {
-  std::unique_lock lock(this->data_.mutex);
-  this->data_.not_empty.wait_(lock, [&] {
-    return !this->data_.queue.is_empty() || this->data_.closed;
-  });
-  if (this->data_.closed) {
+  std::unique_lock lock(this->mutex);
+  this->not_empty.wait(lock,
+                       [&] { return !this->queue.is_empty() || this->closed; });
+  if (this->closed) {
     return std::unexpected("channel closed");
   }
-  auto value = std::move(*this->data_.queue.front());
-  this->data_.queue.pop();
-  this->data_.not_full.notify_one();
+  auto value = std::move(*this->queue.front());
+  this->queue.pop();
+  this->not_full.notify_one();
   return value;
 }
 
 template <typename T, std::size_t Capacity>
 std::expected<T, Error> Channel<T, Capacity>::try_receive() {
-  std::unique_lock lock(this->data_.mutex);
-  if (this->data_.closed) {
+  std::unique_lock lock(this->mutex);
+  if (this->closed) {
     return std::unexpected("channel closed");
   }
-  if (this->data_.queue.is_empty()) {
+  if (this->queue.is_empty()) {
     return std::unexpected(
         "cannot receive inmediatly since the buffer is empty");
   }
-  auto value = std::move(*this->data_.queue.front());
-  this->data_.queue.pop();
-  this->data_.not_empty.notify_one();
+  auto value = std::move(*this->queue.front());
+  this->queue.pop();
+  this->not_empty.notify_one();
   return value;
 }
 } // namespace chx::buffered
